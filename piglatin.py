d9c13b413544a50b63bd7a690965b36c65f2e282
Yianni Skilitis <yskilitis@gmail.com>
2020-02-21 09:42:43 -0500

    added all the rules for single words

--- piglatin.py
+++ piglatin.py
@@ -1,17 +1,22 @@
 print("Pig Latin Translator v1.0 by John Skilitis")
 print("\n\n\n" + "Please enter an English word:")
-
 ''' Initial basic implementation
 pig_word = eng_word[1:] + eng_word[0] + "ay"
 print(pig_word)
 '''
 vowel_list = ['a', 'e', 'i', 'o', 'u', 'y']
+vowel_index = 0 
+
 eng_word = input()
 
-for letter in eng_word:
-    if letter in vowel_list:
-        pig_word = eng_word + "way"
+while True:
+    if eng_word[vowel_index] in vowel_list:
+        if vowel_index == 0:
+            pig_word = eng_word + "way"
+            break
+        else:
+            pig_word = eng_word[vowel_index:] + eng_word[0:vowel_index] + "ay"
+        break
     else:
-        pig_word = eng_word[1:] + eng_word[0] + "ay"
-
+        vowel_index+=1
 print(pig_word)
