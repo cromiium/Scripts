f2cd5ee76c08f7a2471cde0318b4ba5b361a051b
Yianni Skilitis <yskilitis@gmail.com>
2020-02-21 09:20:04 -0500

    piglatin.py warm up script works on single words

--- piglatin.py
+++ piglatin.py
@@ -1,7 +1,17 @@
 print("Pig Latin Translator v1.0 by John Skilitis")
 print("\n\n\n" + "Please enter an English word:")
 
+''' Initial basic implementation
+pig_word = eng_word[1:] + eng_word[0] + "ay"
+print(pig_word)
+'''
+vowel_list = ['a', 'e', 'i', 'o', 'u', 'y']
 eng_word = input()
 
-pig_word = eng_word[1:] + eng_word[0] + "ay"
+for letter in eng_word:
+    if letter in vowel_list:
+        pig_word = eng_word + "way"
+    else:
+        pig_word = eng_word[1:] + eng_word[0] + "ay"
+
 print(pig_word)
